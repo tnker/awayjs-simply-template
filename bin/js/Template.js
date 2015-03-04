2caa647d7f32876a07bd07f3945dba2aea7729a2
tnker <yuuya@tnker.com>
2015-03-04 13:50:26 +0900

    毎回ファイル名変えるの面倒なので、基本構成一緒に...

--- bin/js/Template.js
+++ bin/js/Template.js
@@ -1,7 +1,7 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /*
 
-Sample001 code
+template class
 
 */
 // awayjs-core
@@ -14,8 +14,8 @@ var PrimitiveSpherePrefab = require("awayjs-display/lib/prefabs/PrimitiveSphereP
 // awayjs-methodmaterials
 var MethodMaterial = require("awayjs-methodmaterials/lib/MethodMaterial");
 var MethodRendererPool = require("awayjs-methodmaterials/lib/pool/MethodRendererPool");
-var Sample001 = (function () {
-    function Sample001() {
+var Template = (function () {
+    function Template() {
         var _this = this;
         // 土台生成
         this._view = new View(new DefaultRenderer(MethodRendererPool));
@@ -36,25 +36,33 @@ var Sample001 = (function () {
         this._timer = new RequestAnimationFrame(this.onEnterFrame, this);
         this._timer.start();
     }
-    Sample001.prototype.onEnterFrame = function (dt) {
+    /**
+     * 毎フレーム描画処理
+     * @param {number} dt
+     */
+    Template.prototype.onEnterFrame = function (dt) {
         this._mesh.rotationY += 1;
         this._view.render();
     };
-    Sample001.prototype.onResize = function (event) {
-        if (event === void 0) { event = null; }
+    /**
+     * ブラウザリサイズ時処理
+     * @param {UIEvent} e
+     */
+    Template.prototype.onResize = function (e) {
+        if (e === void 0) { e = null; }
         this._view.x = 0;
         this._view.y = 0;
         this._view.width = window.innerWidth;
         this._view.height = window.innerHeight;
     };
-    return Sample001;
+    return Template;
 })();
 window.onload = function () {
-    new Sample001();
+    new Template();
 };
 
 
 },{"awayjs-core/lib/utils/RequestAnimationFrame":undefined,"awayjs-display/lib/containers/View":undefined,"awayjs-display/lib/prefabs/PrimitiveSpherePrefab":undefined,"awayjs-methodmaterials/lib/MethodMaterial":undefined,"awayjs-methodmaterials/lib/pool/MethodRendererPool":undefined,"awayjs-renderergl/lib/DefaultRenderer":undefined}]},{},[1])
 
 
-//# sourceMappingURL=Sample001.js.map
+//# sourceMappingURL=Template.js.map
--- src/Template.ts
+++ src/Template.ts
@@ -5,57 +5,81 @@ template class
 */
 
 // awayjs-core
-import Vector3D             = require("awayjs-core/lib/geom/Vector3D");
 import RequestAnimationFrame= require("awayjs-core/lib/utils/RequestAnimationFrame");
+// awayjs-renderergl
+import DefaultRenderer      = require('awayjs-renderergl/lib/DefaultRenderer');
 // awayjs-display
-import View                 = require("awayjs-display/lib/containers/View");
+import View                 = require('awayjs-display/lib/containers/View');
 import Mesh                 = require("awayjs-display/lib/entities/Mesh");
-import PrimitivePlanePrefab = require("awayjs-display/lib/prefabs/PrimitivePlanePrefab");
+import PrimitiveSpherePrefab= require("awayjs-display/lib/prefabs/PrimitiveSpherePrefab");
 // awayjs-methodmaterials
 import MethodMaterial       = require("awayjs-methodmaterials/lib/MethodMaterial");
 import MethodRendererPool   = require("awayjs-methodmaterials/lib/pool/MethodRendererPool");
-// awayjs-renderergl
-import DefaultRenderer      = require("awayjs-renderergl/lib/DefaultRenderer");
 
 class Template
 {
+    /**
+     * Canvas相当の描画用ステージ
+     */
     private _view: View;
 
-    private _planeMaterial: MethodMaterial;
+    /**
+     * 表示オブジェクトに付与するマテリアル
+     */
+    private _material: MethodMaterial;
 
-    private _plane: Mesh;
+    /**
+     * 表示オブジェクトの形状情報
+     */
+    private _prefab: PrimitiveSpherePrefab;
 
+    /**
+     * 実際にviewにaddを行う表示オブジェクト
+     */
+    private _mesh: Mesh;
+
+    /**
+     * 描画／アニメーション用描画処理
+     */
     private _timer: RequestAnimationFrame;
 
     constructor()
     {
-        // view setup
+        // 土台生成
         this._view = new View(new DefaultRenderer(MethodRendererPool));
-        // view setting
-        this._view.camera.z = -1000;
-        this._view.camera.y = 500;
-        this._view.camera.lookAt(new Vector3D());
-        // material setup
-        this._planeMaterial = new MethodMaterial();
-        // scene setup
-        this._plane = <Mesh> new PrimitivePlanePrefab(700, 700).getNewObject();
-        this._plane.material = this._planeMaterial;
-        this._view.scene.addChild(this._plane);
-        // bind resize
-        window.onresize = (event: UIEvent) => this.onResize(event);
+        // リサイズ時処理
+        window.onresize = (e: UIEvent): void => this.onResize(e);
+        // 初回サイズ設定
         this.onResize();
-        // enterframe start
+        // マテリアル生成
+        this._material = new MethodMaterial(0xFF0000);
+        // 形状生成
+        this._prefab = new PrimitiveSpherePrefab(300);
+        // メッシュ取得
+        this._mesh = <Mesh> this._prefab.getNewObject();
+        this._mesh.material = this._material;
+        // ステージ上に追加
+        this._view.scene.addChild(this._mesh);
+        // アニメーション用の毎フレーム描画処理
         this._timer = new RequestAnimationFrame(this.onEnterFrame, this);
         this._timer.start();
     }
 
+    /**
+     * 毎フレーム描画処理
+     * @param {number} dt
+     */
     private onEnterFrame(dt: number): void
     {
-        this._plane.rotationY += 1;
+        this._mesh.rotationY += 1;
         this._view.render();
     }
 
-    private onResize(event: UIEvent = null): void
+    /**
+     * ブラウザリサイズ時処理
+     * @param {UIEvent} e
+     */
+    private onResize(e: UIEvent = null): void
     {
         this._view.x = 0;
         this._view.y = 0;
